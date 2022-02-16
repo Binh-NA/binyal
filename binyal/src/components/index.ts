265899bb142db2ee8a72ca81caa3cd8345ca2487
Binh-NA <nguyenanbinh10a13@gmail.com>
2022-02-16 22:20:21 +0700

    Input component

--- binyal/src/components/index.ts
+++ binyal/src/components/index.ts
@@ -1,6 +1,7 @@
 import Space from './space';
 
 export { default as Button } from './button';
+export { default as Input } from './input';
 export { default as Loading } from './loading';
 export const SpaceX = Space.SpaceX;
 export const SpaceY = Space.SpaceY;
